3f375a60caaf5d4f8b66991fe451a692c56f4d65
Justen Manni <me@justenmanni.dev>
2023-09-16 18:59:55 +0800

    Add User Entity

---
+++ src/main/java/com/springmart/springmartbackend/entity/User.java
@@ -0,0 +1,47 @@
+package com.springmart.springmartbackend.entity;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "user")
+@Data
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Long id;
+
+    @Column(name = "email")
+    String email;
+
+    @Column(name = "password")
+    String password;
+
+    @Column(name = "first_name")
+    String firstName;
+
+    @Column(name = "last_name")
+    String lastName;
+
+    @Column(name = "address")
+    String address;
+
+    @Column(name = "opt_marketing")
+    boolean optMarketing;
+
+    @Column(name = "join_date")
+    LocalDate joinDate;
+}
