153f8369c774fcd68a6ae5afcb151439e1d17b1c
Justen Manni <me@justenmanni.dev>
2023-09-17 13:53:34 +0800

    Add jakarta persistence constraints to SpringUser class

--- src/main/java/com/springmart/springmartbackend/entity/SpringUser.java
+++ src/main/java/com/springmart/springmartbackend/entity/SpringUser.java
@@ -11,6 +11,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,30 +31,41 @@ public class SpringUser {
     @Column(name = "id")
     Long id;
 
-    @Column(name = "email")
+    @Column(name = "email", nullable = false, unique = true)
+    @NotNull(message = "Email is required")
+    @Email(message = "Email should be valid")
     String email;
 
-    @Column(name = "password")
+    @Column(name = "password", nullable = false)
+    @NotNull(message = "Password is required")
     String password;
 
-    @Column(name = "first_name")
+    @Column(name = "first_name", nullable = false)
+    @NotNull(message = "First name is required")
     String firstName;
 
-    @Column(name = "last_name")
+    @Column(name = "last_name", nullable = false)
+    @NotNull(message = "Last name is required")
     String lastName;
 
-    @Column(name = "address")
+    @Column(name = "address", nullable = false)
+    @NotNull(message = "Address is required")
     String address;
 
-    @Column(name = "postal_code")
+    @Column(name = "postal_code", nullable = false)
+    @NotNull(message = "Postalcode is required")
     String postalCode;
 
-    @Column(name = "unit_no")
+    @Column(name = "unit_no", nullable = false)
+    @NotNull(message = "Unit no is required")
     String unitNo;
 
-    @Column(name = "opt_marketing")
+    @Column(name = "opt_marketing", nullable = false)
+    @NotNull(message = "Opt for marketing is required")
     boolean optMarketing;
 
-    @Column(name = "join_date")
+    @Column(name = "join_date", nullable = false)
+    @NotNull(message = "Registration date is required")
+    @PastOrPresent(message = "Registration date should not be in the future")
     LocalDate joinDate;
 }
