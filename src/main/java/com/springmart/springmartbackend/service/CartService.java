c79e41052077d21ef3d33648372b686bbd004c88
Parry <Admin@Admins-MacBook-Pro-8.local>
2023-09-17 17:49:36 +0800

    Cart Service and Cart Service implementation done

--- src/main/java/com/springmart/springmartbackend/service/CartService.java
+++ src/main/java/com/springmart/springmartbackend/service/CartService.java
@@ -1,5 +1,15 @@
 package com.springmart.springmartbackend.service;
 
-public class CartService {
+import java.util.ArrayList;
+
+import com.springmart.springmartbackend.dto.Cart;
+
+public interface CartService {
+
+    Cart createCart(Cart cart);
+    Cart getCart(Long id);
+    ArrayList<Cart> getAllCarts();
+    void deleteCart(Long id);
+
     
 }
