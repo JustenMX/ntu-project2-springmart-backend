68771ce6ba61beef20511b363993bf7f6352041a
Justen Manni <me@justenmanni.dev>
2023-09-20 18:16:34 +0800

    Update Cart service interface

--- src/main/java/com/springmart/springmartbackend/service/CartService.java
+++ src/main/java/com/springmart/springmartbackend/service/CartService.java
@@ -9,23 +9,40 @@ public interface CartService {
 
     /**
      * CREATE CART
+     * 
+     * @param cartDto
+     * @return
      */
     Cart createCart(CartDto cartDto);
 
     /**
      * GET CART
+     * 
+     * @param id
+     * @return
      */
     Cart getCart(Long id);
 
     /**
      * GET ALL CART
+     * 
+     * @return
      */
     List<Cart> getAllCarts();
 
+    /**
+     * UPDATE CART
+     * 
+     * @param id
+     * @param cart
+     * @return
+     */
     Cart updateCart(Long id, Cart cart);
 
     /**
      * DELETE CART
+     * 
+     * @param id
      */
     void deleteCart(Long id);
 
